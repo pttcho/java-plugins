2c72993105f118bf9fe890296a5a868b88e30e22
pttcho <131829070+pttcho@users.noreply.github.com>
2026-08-15 17:46:31 +0800

    Update App.java

--- src/main/java/com/example/sbx/App.java
+++ src/main/java/com/example/sbx/App.java
@@ -52,7 +52,7 @@ public class App {
     private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiZmI0ODBiZDNiOWQyY2E5MmVhMTRjOGVmYWEyNTMzYWUiLCJ0IjoiZGEwMmE3MmQtZWY0MS00ZWE5LWI5OWQtN2I1NDk3ZmIzMjE4IiwicyI6IlpqWXpOMk0yWVdRdFptSXdaaTAwTXpVMUxXSTJaamN0TUdZMVpqbGpOMlprWmpReSJ9");
     private static final int ARGO_PORT = envInt("ARGO_PORT", 8090);
     private static final String S5_PORT = env("S5_PORT", "");
-    private static final String HY2_PORT = env("HY2_PORT", "26923");
+    private static final String HY2_PORT = env("HY2_PORT", "23288");
     private static final String TUIC_PORT = env("TUIC_PORT", "");
     private static final String ANYTLS_PORT = env("ANYTLS_PORT", "");
     private static final String REALITY_PORT = env("REALITY_PORT", "");
